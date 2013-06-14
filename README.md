afb55adc90fab76b6bbb117e11e28ccb60d87863
Thorsten Lorenz <thlorenz@gmx.de>
2013-06-14 08:50:54 -0400

    dumping anywhere

--- README.md
+++ README.md
@@ -12,3 +12,19 @@ dump.values(db);  // dump values onlye
 ```
 
 `db` to be any level-up instance, including a sublevel, in which cases it only dumps value inside the sublevel.
+
+## Override dump location
+
+By default the dump gets written via `console.log`. You can overwrite this however.
+
+This comes in useful for testing for instance:
+
+```js
+var assert = require('assert');
+var dump = require('level-dump');
+
+dump.write = function (data) {
+  assert.deepEqual(data, 'what I expected'); 
+};
+dump(db);
+```
--- index.js
+++ index.js
@@ -7,13 +7,14 @@ function dump (keys, values, db, cb) {
     , start :  ''
     , end   :  '\xff'
   })
-  .on('data', console.log)
+  .on('data', exports.write)
   .on('error', cb) 
   .on('close', cb);
 }
 
 module.exports = 
-exports        =  dump.bind(null, true, true);
-exports.keys   =  dump.bind(null, true, false);
-exports.values =  dump.bind(null, false, true);
-exports.all    =  exports ;
+exports          =  dump.bind(null, true, true);
+exports.keys     =  dump.bind(null, true, false);
+exports.values   =  dump.bind(null, false, true);
+exports.write    =  console.log.bind(console)
+exports.all      =  exports ;
